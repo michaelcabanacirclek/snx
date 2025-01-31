2215ad9a4da0a2c74cbde2fba3cde1d12cb43cb3
Michael Cabana <michael.cabana@circlek.com>
2025-01-31 13:48:51 -0500

    feat(ui): adds hello world component

---
+++ libs/ui/src/lib/ui/components/hello-world.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'snx-hello-world',
+  template: '<h1>Hello World</h1>',
+})
+export class HelloWorldComponent {}
