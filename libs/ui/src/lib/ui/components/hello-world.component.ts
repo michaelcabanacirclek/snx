92c813a822c26573f7be23e6370c0a6b24cf14e8
Michael Cabana <michael.cabana@circlek.com>
2025-01-31 18:24:08 -0500

    fix(ui): typo

--- libs/ui/src/lib/ui/components/hello-world.component.ts
+++ libs/ui/src/lib/ui/components/hello-world.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'snx-hello-world',
-  template: '<h1>Hello Worlds</h1>',
+  template: '<h1>Hello World</h1>',
 })
 export class HelloWorldComponent {}
