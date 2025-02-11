5eb8b10522e4d73bb599a4489f1095319d3f9513
Michael Cabana <michael.cabana@circlek.com>
2025-02-11 16:43:42 -0500

    feat(ui): adds hi alert

--- libs/ui/src/lib/ui/components/hello-world.component.ts
+++ libs/ui/src/lib/ui/components/hello-world.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'snx-hello-world',
-  template: '<h1>Hello World</h1>',
+  template: `<h1>Hello World</h1>
+    <button (click)="sayHello()">Say hi! ✋</button>`,
 })
-export class HelloWorldComponent {}
+export class HelloWorldComponent {
+  sayHello() {
+    alert('HI!');
+  }
+}
