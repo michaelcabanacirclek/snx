5ffe13b9ae7fbeb270e157a81be6ae726a980408
Michael Cabana <michael.cabana@circlek.com>
2025-02-12 11:01:28 -0500

    feat(ui): adds goodbye button

--- libs/ui/src/lib/ui/components/hello-world.component.ts
+++ libs/ui/src/lib/ui/components/hello-world.component.ts
@@ -4,10 +4,15 @@ import { Component } from '@angular/core';
   imports: [],
   selector: 'snx-hello-world',
   template: `<h1>Hello World</h1>
-    <button (click)="sayHello()">Say hi! ✋</button>`,
+    <button (click)="sayHello()">Say hi! ✋</button>
+    <button (click)="sayGoodbye()">Say goodbye!</button>`,
 })
 export class HelloWorldComponent {
   sayHello() {
     alert('Hi World!');
+  }
+
+  sayGoodbye() {
+    alert('Bye!');
   }
 }
